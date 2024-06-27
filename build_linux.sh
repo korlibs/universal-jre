59e08ece1dcf92b9da25a56bee62cba2d52203ff
soywiz <soywiz@gmail.com>
2024-06-27 16:28:35 +0200

    More work

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,5 @@
+#docker build -t build_windows_jre_x64 -f Dockerfile.windows.x64 .
+
 docker build -t build_linux_jre_x64 -f Dockerfile.linux.x64 .
 container_id=$(docker create "build_linux_jre_x64")
 docker cp $container_id:/home/build/microsoft-jre-21.0.3-linux-x64.tar.xz microsoft-jre-21.0.3-linux-x64.tar.xz
